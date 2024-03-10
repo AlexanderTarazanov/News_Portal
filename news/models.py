16abc5fe593ab864ea78ddf74ab2d70644f68bf4
AlexanderTarazanov <alextarazanov@gmail.com>
2024-03-10 16:35:53 +0300

    Add files via upload

---
+++ news/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum
+from django.urls import reverse
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
+
+    def update_rating(self):
+        post_rating = Post.objects.filter(author=self).aggregate(
+            Sum('rating'))['rating__sum']
+        comment_rating = Comment.objects.filter(user=self.user).aggregate(
+            Sum('rating'))['rating__sum']
+        comment_rating_to_posts = Comment.objects.filter(
+            post__author__user=self.user).aggregate(Sum('rating'))['rating__sum']
+
+        self.rating = ((post_rating * 3) + comment_rating + comment_rating_to_posts)
+        self.save()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    subscribers = models.ManyToManyField(User, blank=True, null=True, related_name='categories')
+
+    def __str__(self):
+        return self.name.title()
+
+
+class Post(models.Model):
+    news = "NW"
+    articles = "AR"
+
+    POST_TYPES = [
+        (news, "Новость"),
+        (articles, "Статья")
+    ]
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post_type = models.CharField(max_length=10, choices=POST_TYPES, default=news)
+    created_time = models.DateTimeField(auto_now_add=True)
+    categories = models.ManyToManyField(Category, through='PostCategory', related_name='PostCategory')
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.title.title()}: {self.text[:20]}'
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
+
+    def preview(self):
+        return f'{self.text[:124]}...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating += 1
+        self.save()
+
+    def dislike(self):
+        self.rating -= 1
+        self.save()
